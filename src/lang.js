b17378b1de779682b48d54eeb82a8d6e76145b92
Manuel Porto <manu.porto94@gmail.com>
2017-09-29 12:12:18 -0300

    Add valid rule format definition

--- src/lang.js
+++ src/lang.js
@@ -1,5 +1,6 @@
 var Lang = {
-    validFactFormat: /^[a-z]+\(([a-z]+, )*[a-z]+\)/, 
+    validFactFormat: /^[a-z]+\(([a-z]+, )*[a-z]+\)/,
+    validRuleFormat: /^[a-z]+\(([A-Z]+, )*[A-Z]+\) :- (([a-z]+\(([A-Z]+, )*[A-Z]+\)), )*([a-z]+\(([A-Z]+, )*[A-Z]+\))/
 }
 
 module.exports = Lang;
