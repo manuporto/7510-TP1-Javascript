1dc407294d26a576e4d3627893fb2101d4f2c0ac
Manuel Porto <manu.porto94@gmail.com>
2017-10-01 16:33:44 -0300

    Add parseRule method

--- src/parser/ruleParser.js
+++ src/parser/ruleParser.js
@@ -1,7 +1,12 @@
 var Lang = require('../lang');
+var Rule = require('../entities/rule');
+var FactParser = require('../parser/factParser');
 
 var RuleParser = function() {
     
+    this.factParser = new FactParser();
+    var self = this;
+
     this.validRule = function(rule) {
         return Lang.validRuleFormat.test(rule);
     };
@@ -10,11 +15,24 @@ var RuleParser = function() {
         return rule.substr(0, rule.indexOf(Lang.openArg));
     };
 
+    this.getRuleArgs = function(rule){
+        return rule
+        .substring(rule.indexOf(Lang.openArg) + 1, rule.indexOf(Lang.closeArg))
+        .split(Lang.argSep);
+    };
+
     this.getRuleFacts = function(rule) {
         return rule
         .replace(Lang.defFactSep, Lang.customFactSep)
         .substring(rule.indexOf(Lang.ruleEq) + Lang.ruleEq.length, rule.indexOf(Lang.lineEnd))
         .split(Lang.factSep);
+    };
+
+    this.parseRule = function(rule) {
+        var parsedFacts = this.getRuleFacts(rule).map(function(fact) {
+            return self.factParser.parseFact(fact);
+        });
+        return new Rule(this.getRuleName(rule), this.getRuleArgs(rule), parsedFacts);
     };
 };
 
