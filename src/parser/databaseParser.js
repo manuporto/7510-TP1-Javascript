3db5da7e18ad1a3a9d8e3e1fe5fe6afa30b580ac
Manuel Porto <manu.porto94@gmail.com>
2017-10-01 12:34:22 -0300

    Add tests and fixes

--- src/parser/databaseParser.js
+++ src/parser/databaseParser.js
@@ -5,10 +5,11 @@ var RuleParser = require('./ruleParser');
 var DatabaseParser = function() {
     this.factParser = new FactParser();
     this.ruleParser = new RuleParser();
+    var self = this;
 
     this.validDatabase = function(db) {
         return !db.map(function(line) {
-            return (factParser.validFact(line) || ruleParser.validRule(line));
+            return (self.factParser.validFact(line) || self.ruleParser.validRule(line));
         }).includes(false);
     };
 };
--- test/parser/databaseParserTests.js
+++ test/parser/databaseParserTests.js
@@ -7,14 +7,41 @@ var DatabaseParser = require('../../src/parser/databaseParser');
 describe('DatabaseParser', function() {
 
     var databaseParser;
+    var validDatabase = [
+        'male(seldon).',
+        'male(toran).',
+        'female(arkady).',
+        'father(toran, arkady).',
+        'daughter(X, Y) :- female(X), father(Y, X).'
+    ];
+    var databaseWithInvalidFact = [
+        'male(seldon.',
+        'male(toran).',
+        'female(arkady).',
+        'father(toran, arkady).',
+        'daughter(X, Y) :- female(X), father(Y, X).'  
+    ];
+    var databaseWithInvalidRule = [
+        'male(seldon).',
+        'male(toran).',
+        'female(arkady).',
+        'father(toran, arkady).',
+        'daughter(X, Y) :- female(X) father(Y, X).'  
+    ]
 
     beforeEach(function() {
         databaseParser = new DatabaseParser();
     });
 
-    describe.skip('#validDatabase()', function() {
+    describe('#validDatabase()', function() {
+
         it('should return true when a valid database its evaluated', function() {
-            expect(true).to.be.true;
+            expect(databaseParser.validDatabase(validDatabase)).to.be.true;
         });
+
+        it('should return false when an invalid database its evaluated', function() {
+            expect(databaseParser.validDatabase(databaseWithInvalidFact)).to.be.false;
+            expect(databaseParser.validDatabase(databaseWithInvalidRule)).to.be.false;
+        })
     });
 });
