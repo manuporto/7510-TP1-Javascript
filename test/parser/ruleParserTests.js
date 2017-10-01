39506642026c2d74d4a56fc42ce349940e8a9a75
Manuel Porto <manu.porto94@gmail.com>
2017-10-01 15:45:55 -0300

    Add tests for new methods

--- test/parser/ruleParserTests.js
+++ test/parser/ruleParserTests.js
@@ -7,6 +7,7 @@ var RuleParser = require('../../src/parser/ruleParser');
 describe('RuleParser', function() {
 
     var ruleParser;
+    var validRule = 'son(X, Y) :- male(X), father(Y, X).'
     
     beforeEach(function() {
         ruleParser = new RuleParser();
@@ -15,13 +16,26 @@ describe('RuleParser', function() {
     describe('#validRule()', function() {
 
         it('should return true when a valid rule is evaluated', function() {
-            expect(ruleParser.validRule('son(X, Y) :- male(X), father(Y, X).')).to.be.true;
+            expect(ruleParser.validRule(validRule)).to.be.true;
             expect(ruleParser.validRule('son(X, Y) :- male(X), male(Y), father(Y, X).')).to.be.true;
         });
 
         it('should return false when an invalid rule is evaluated', function() {
             expect(ruleParser.validRule('son(X,Y :- male(X), father(Y, X).')).to.be.false;
             expect(ruleParser.validRule('son(X,Y) : male(X), father(Y, X).')).to.be.false;
-        })
+        });
+    });
+
+    describe('#getRuleName()', function() {
+        it('should return the rule\'s name', function() {
+            expect(ruleParser.getRuleName(validRule)).to.equal('son');
+        });
+    });
+
+    describe('#getRuleFacts()', function() {
+        it('should return rule\'s facts as an array of strings containing them', function() {
+            expect(ruleParser.getRuleFacts(validRule))
+            .to.eql(['male(X)', 'father(Y, X)']);
+        });
     });
 });
