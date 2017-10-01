c969ebae22b6bd7e639ae5777df9188de9df79cd
Manuel Porto <manu.porto94@gmail.com>
2017-10-01 11:08:46 -0300

    Delete getter functions

--- src/entities/fact.js
+++ src/entities/fact.js
@@ -1,14 +1,6 @@
 var Fact = function(name, args) {
     this.name = name;
     this.args = args;
-
-    this.getName = function() {
-        return this.name;
-    };
-
-    this.getArgs = function() {
-        return this.args;
-    };
 };
 
 module.exports = Fact;
