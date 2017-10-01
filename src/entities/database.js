d9329613157ca6ccd36bf6c1a17c6e66f4684f4c
Manuel Porto <manu.porto94@gmail.com>
2017-10-01 13:49:53 -0300

    Create  Database entity

---
+++ src/entities/database.js
@@ -0,0 +1,8 @@
+var Database = function(facts, factNames, rules, ruleNames) {
+    this.facts = facts;
+    this.factNames = factNames;
+    this.rules = rules;
+    this.ruleNames = ruleNames;
+}
+
+module.exports = Database;
