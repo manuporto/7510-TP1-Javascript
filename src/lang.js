c9133bc854be5f89039c5e35eea595e702307983
Manuel Porto <manu.porto94@gmail.com>
2017-10-01 01:58:08 -0300

    Add langauge definitions

--- src/lang.js
+++ src/lang.js
@@ -1,4 +1,10 @@
 var Lang = {
+    openArg: '(',
+    closeArg: ')',
+    argSep: ', ',
+    lineEnd: '.',
+    ruleEq: ':-',
+    factSep: ', ',
     validFactFormat: /^[a-z]+\(([a-z]+, )*[a-z]+\)/,
     validRuleFormat: /^[a-z]+\(([A-Z]+, )*[A-Z]+\) :- (([a-z]+\(([A-Z]+, )*[A-Z]+\)), )*([a-z]+\(([A-Z]+, )*[A-Z]+\))/
 }
