ed70f1966fb614d2924dd273f0b74460ec83f816
Manuel Porto <manu.porto94@gmail.com>
2017-10-01 15:45:48 -0300

    Add getRuleName and getRuleFacts methods

--- src/parser/ruleParser.js
+++ src/parser/ruleParser.js
@@ -5,6 +5,17 @@ var RuleParser = function() {
     this.validRule = function(rule) {
         return Lang.validRuleFormat.test(rule);
     };
+
+    this.getRuleName = function(rule) {
+        return rule.substr(0, rule.indexOf(Lang.openArg));
+    };
+
+    this.getRuleFacts = function(rule) {
+        return rule
+        .replace(Lang.defFactSep, Lang.customFactSep)
+        .substring(rule.indexOf(Lang.ruleEq) + Lang.ruleEq.length, rule.indexOf(Lang.lineEnd))
+        .split(Lang.factSep);
+    };
 };
 
 module.exports = RuleParser;
