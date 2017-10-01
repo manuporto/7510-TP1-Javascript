7c1db27026597e81bf9a3bac3f71a040f68efa11
Manuel Porto <manu.porto94@gmail.com>
2017-10-01 01:58:49 -0300

    Parse facts

--- src/parser/factParser.js
+++ src/parser/factParser.js
@@ -1,10 +1,24 @@
 var Lang = require('../lang');
+var Fact = require('../entities/fact');
 
 var FactParser = function() {
 
     this.validFact = function(fact) {
         return Lang.validFactFormat.test(fact);
     };
+
+    this.getFactName = function(fact) {
+        return fact.substr(0, fact.indexOf(Lang.openArg));
+    };
+
+    this.getFactArgs = function(fact) {
+        console.log(fact.substr(fact.indexOf(Lang.openArg++), fact.indexOf(Lang.closeArg)));
+        return fact.substr(fact.indexOf(Lang.openArg++), fact.indexOf(Lang.closeArg)).split(Lang.argSep);
+    };
+
+    this.parseFact = function(fact) {
+        return new Fact(this.getFactName(fact), this.getFactArgs(fact));
+    }
 };
 
 module.exports = FactParser;
