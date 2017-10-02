1a80dcfc9296f9cf755a8e9857a224ab5a03bcd2
Manuel Porto <manu.porto94@gmail.com>
2017-10-02 16:56:12 -0300

    Add db parsing and fact checking

--- src/entities/database.js
+++ src/entities/database.js
@@ -1,8 +1,25 @@
+var Fact = require('./fact');
+
 var Database = function(facts, factNames, rules, ruleNames) {
     this.facts = facts;
     this.factNames = factNames;
     this.rules = rules;
     this.ruleNames = ruleNames;
-}
+
+    this.factExists = function(qfact) {
+        return this.facts.filter(function(fact) {
+            return fact.equals(qfact);
+        }).length > 0;
+    };
+
+    this.evaluateQuery = function(query) {
+        if (this.factNames.has(query.name)) {
+            return this.factExists(new Fact(query.name, query.args));
+        } else if (this.ruleNames.has(query.name)) {
+            return true;
+        }
+        return false;
+    };
+};
 
 module.exports = Database;
--- src/interpreter.js
+++ src/interpreter.js
@@ -1,13 +1,22 @@
+var DatabaseParser = require('./parser/databaseParser');
+var Database = require('./entities/database');
+var QueryParser = require('./parser/queryParser');
+
 var Interpreter = function () {
 
-    this.parseDB = function (params, paramss, paramsss) {
+    this.database;
+    this.queryParser = new QueryParser();
 
+    this.parseDB = function (db) {
+        var databaseParser = new DatabaseParser();
+        this.database = databaseParser.createDatabase(db);
     }
 
-    this.checkQuery = function (params) {
-        return true;
-    }
-
-}
+    this.checkQuery = function (query) {
+        if (this.queryParser.validQuery(query)) {
+            return this.database.evaluateQuery(this.queryParser.parseQuery(query));
+        };
+    };
+};
 
 module.exports = Interpreter;
