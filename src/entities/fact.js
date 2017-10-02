8da3743487f5189cd2bd46e69e9c0078832481a8
Manuel Porto <manu.porto94@gmail.com>
2017-10-02 16:54:37 -0300

    Add equals function to Fact.prototype

--- src/entities/fact.js
+++ src/entities/fact.js
@@ -1,10 +1,9 @@
 var Fact = function(name, args) {
     this.name = name;
     this.args = args;
-
-    this.equals = function(fact) {
-        return this.name === fact.name && this.args.join(',') === fact.args.join(',');
-    }
 };
 
+Fact.prototype.equals = function(fact) {
+    return this.name === fact.name && this.args.join(',') === fact.args.join(',');
+}
 module.exports = Fact;
